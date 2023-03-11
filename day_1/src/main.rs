6a6460c03eb2dd58b1d77df396c36fa2a886524b
Jose Miguel Lozano <jmlozano98@gmail.com>
2023-03-11 14:13:21 +0100

    Day 1 done

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,32 @@
+
+fn get_calorie_vectors(content: String) -> Vec<Vec<u32>> {
+    let mut result: Vec<Vec<u32>> = vec![];
+    let mut vector: Vec<u32> = vec![];
+    for line in content.lines() {
+        if line.eq("") {
+            result.push(vector.to_vec());
+            vector.clear();
+        } else {
+            vector.push(line.parse()
+                        .expect("Line could not be parsed"));
+        }
+    }
+    return result;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_name = "resources/calories.txt";
+    let file_content = std::fs::read_to_string(file_name)
+        .expect("Could not read file");
+    let vectors = get_calorie_vectors(file_content);
+    let max: u32 = vectors
+        .iter()
+        .map(|cals| cals
+             .iter()
+             .sum())
+        .max()
+        .unwrap();
+
+    println!("Max calories: {}", max);
+    println!("done.");
 }
